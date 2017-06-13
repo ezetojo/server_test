78193850b0df6bb2f84c6c625b9076c012a51409
Eze Tojo <eze.tojo@gmail.com>
2017-06-13 18:54:37 -0300

    Set port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = 'localhost';
-const port = 8080;
+// const hostname = 'localhost';
+const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -9,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at port: ${port}`);
 });
